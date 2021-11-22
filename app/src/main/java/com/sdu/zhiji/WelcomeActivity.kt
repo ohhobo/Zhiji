7847c975d1d8130928069dfbc4c93831600ade9a
ohhobo <1301868714@qq.com>
2021-11-22 15:32:55 +0800

    2021-11-22-15:33

--- app/src/main/java/com/sdu/zhiji/WelcomeActivity.kt
+++ app/src/main/java/com/sdu/zhiji/WelcomeActivity.kt
@@ -1,11 +1,20 @@
 package com.sdu.zhiji
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_welcome)
+        val button1:Button=findViewById(R.id.button1)
+        val intent=Intent(this,MainActivity::class.java)
+        Handler().postDelayed({startActivity(intent)},5000)
+        button1.setOnClickListener {
+            startActivity(intent)
+        }
     }
 }
