559e4265c085360aea813136ac5a9c12cde2b77d
ohhobo <1301868714@qq.com>
2021-11-28 13:44:14 +0800

    2021-11-28 定制 finished

--- app/src/main/java/com/sdu/zhiji/ui/make/MakeFragment.kt
+++ app/src/main/java/com/sdu/zhiji/ui/make/MakeFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.sdu.zhiji.R
 import com.sdu.zhiji.databinding.FragmentMakeBinding
 
 class MakeFragment : Fragment() {
@@ -30,10 +33,43 @@ class MakeFragment : Fragment() {
         _binding = FragmentMakeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textMake
-        makeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+//        val textView: TextView = binding.textMake
+//        makeViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+
+        val progressbar:ProgressBar=root.findViewById(R.id.progressBar_make)
+        val text:TextView=root.findViewById(R.id.textView_make3)
+        val image_ps:ImageView=root.findViewById(R.id.ps_image)
+        text.setOnClickListener{
+            if(progressbar.progress!=100) {
+                progressbar.progress=progressbar.progress+10
+            }
+            else{
+                progressbar.progress=0
+            }
+
+        }
+        progressbar.setOnClickListener{
+            if(progressbar.progress!=100) {
+                progressbar.progress=progressbar.progress+10
+            }
+            else{
+                progressbar.progress=0
+            }
+
+        }
+        image_ps.setOnClickListener{
+            if(progressbar.progress!=100) {
+                progressbar.progress=progressbar.progress+10
+            }
+            else{
+                progressbar.progress=0
+            }
+
+        }
+
+
         return root
     }
 
