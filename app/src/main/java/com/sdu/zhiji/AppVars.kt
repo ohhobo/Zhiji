025464af76bf6668b438e76529a5c71109c1b851
ides0713 <1229139809@qq.com>
2021-12-03 09:34:48 +0800

    2021-11-22-15:33

--- app/src/main/java/com/sdu/zhiji/AppVars.kt
+++ app/src/main/java/com/sdu/zhiji/AppVars.kt
@@ -18,6 +18,10 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.io.InputStreamReader
+import java.net.Socket
+import java.util.*
+import kotlin.concurrent.thread
 
 val retrofit: Retrofit = Retrofit.Builder()
     .baseUrl("http://81.68.226.148/")
@@ -26,7 +30,68 @@ val retrofit: Retrofit = Retrofit.Builder()
 
 object SignStatus {
     var status: Int = 0
-    var username:String=""
+    var username: String = ""
+}
+
+class IConnect(ipAddress: String, port: Int) {
+    private val ip = ipAddress
+    private val p = port
+    private var sc: Socket? = null
+    private var status: Int = -1
+
+    init {
+        sc = Socket(ip, p)
+        if (sc != null) {
+            Log.d("iconnect","connect server successfully")
+        } else {
+            Log.d("iconnect","connect fail now retry")
+            while (!retry()) {
+            }
+        }
+    }
+
+    private fun retry(): Boolean {
+        if (sc == null) {
+            sc = Socket(ip, p)
+            return sc != null
+        }
+        return true
+    }
+
+    fun sendR(req: String) {
+        val outStream = sc!!.getOutputStream()
+        val msg = req.toByteArray()
+        outStream.write(msg)
+        outStream.flush()
+    }
+
+    fun recvR(): String? {
+        val inStream = sc!!.getInputStream()
+        val dataIn = InputStreamReader(inStream, "gb2312")
+        val inMessage = CharArray(1024)
+        val a = dataIn.read(inMessage)
+        if (a <= -1)
+            return null
+        return String(inMessage, 0, a)
+    }
+
+//    fun mainProcess() {
+//        thread {
+//            val scan = Scanner(System.`in`)
+//            while (true) {
+//                val string: String = scan.nextLine()
+//                sendR(string);
+//            }
+//        }
+//        thread {
+//            while (true) {
+//                val string: String? = recvR()
+//                if (string != null)
+//                    println(string)
+//                else continue
+//            }
+//        }
+//    }
 }
 
 //api for sign_in
--- app/src/main/java/com/sdu/zhiji/MainActivity.kt
+++ app/src/main/java/com/sdu/zhiji/MainActivity.kt
@@ -3,21 +3,28 @@ package com.sdu.zhiji
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.widget.Button
 import android.widget.Toast
+import kotlin.math.sign
 import kotlin.math.truncate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button1 = findViewById<Button>(R.id.chat_button)
+        button1.setOnClickListener {
+                val intent = Intent(this, ChatActivity::class.java)
+                startActivity(intent)
+        }
         //点击button2 跳转至登录界面
         val button2 = findViewById<Button>(R.id.signin_button)
         button2.setOnClickListener {
-            if (SignStatus.status == 1) {
-                Toast.makeText(this,"already sign in",Toast.LENGTH_SHORT)
-            } else {
+            if (SignStatus.status == 1)
+                Toast.makeText(this, "already sign in", Toast.LENGTH_SHORT)
+            else {
                 val intent = Intent(this, SigninActivity::class.java)
                 startActivity(intent)
             }
